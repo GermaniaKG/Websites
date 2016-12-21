79679987c10b0b30897fcef7ee68095b32ebff8c
Carsten Witt <carsten.witt@gmail.com>
2016-12-21 14:13:58 +0100

    Added test cases

--- tests/default/WebsitesTest.php
+++ tests/default/WebsitesTest.php
@@ -21,5 +21,38 @@ class WebsitesTest extends \PHPUnit_Framework_TestCase
         $w = $sut->get( "foo ");
     }
 
+    public function testContainerInterfaceGetMethod(  )
+    {
+        $sut = new Websites;
+        $sut->websites['foo'] = "foo";
+
+        $w = $sut->get( "foo ");
+        $this->assertEquals( "foo", $w );
+    }
+
+    public function testContainerInterfaceHasMethod(  )
+    {
+        $sut = new Websites;
+        $this->assertFalse( $sut->has('foo') );
+
+        $sut->websites['foo'] = "foo";
+        $this->assertTrue( $sut->has('foo') );
+    }
+
+    public function testIterator(  )
+    {
+        $sut = new Websites;
+        $this->assertTrue( $sut instanceOf \Traversable );
+
+        $iterator = $sut->getIterator();
+        $this->assertTrue( $iterator instanceOf \Traversable );
+    }
+
+    public function testCountable(  )
+    {
+        $sut = new Websites;
+        $this->assertInternalType( "int", count($sut) );
+    }
+
 
 }
